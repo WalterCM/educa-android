2e0611c6643b134985e6bccb8ea64497185f6d74
Walter Capa <walterc316@gmail.com>
2018-07-07 23:25:34 -0500

    Se agrego proyecto de android studio

---
+++ app/src/main/java/com/instituto/educa/APIContract.java
@@ -0,0 +1,15 @@
+package com.instituto.educa;
+
+/*
+    String URL = "http://192.168.1.4:8000/";
+    String SECRET_KEY = "-)am@px!($c8hs+&d^x61dr9e))b&b%)2@sf)=$4iurzpdgq#!";
+
+    String URL = "http://waltercm.pythonanywhere.com/";
+    String SECRET_KEY = "kz%1kyroab*0t(2f)@^%ap-*abmdqpygdj+kwuk1oea9@o^z-+";
+
+ */
+
+public interface APIContract {
+    String URL = "http://192.168.1.3:8000/";
+    String SECRET_KEY = "-)am@px!($c8hs+&d^x61dr9e))b&b%)2@sf)=$4iurzpdgq#!";
+}
